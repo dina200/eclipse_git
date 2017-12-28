0e2dbdcbf32a559ca4f49d05898a51fb9f2910b7
dina200 <dinapeace@ya.ru>
2017-12-28 20:21:00 +0200

    Polymorphism_Pool

--- HFJ/src/fortest/TestDrive.java
+++ HFJ/src/fortest/TestDrive.java
@@ -1,12 +1,19 @@
 package fortest;
 
+import chap2.Dog;
+import chap2.Movie;
 import game.*;
 
 public class TestDrive {
 
 	public static void main(String[] args) {
-		GuessGame game = new GuessGame();
-		game.startGame();
+//		GuessGame game = new GuessGame();
+//		game.startGame();
+		Dog d = new Dog();
+		System.out.println(d.toString());
+		Movie m = new Movie();
+		System.out.print(m.toString());
+		
 	}
 
 }
---
+++ TaskPool/src/task/Acts.java
@@ -0,0 +1,8 @@
+package task;
+
+public class Acts extends Picasso {
+	public int iMethod() {
+		return 5;
+	}
+
+}
